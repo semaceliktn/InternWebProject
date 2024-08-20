5d5f7da01ce04309c5c8d232c96435bfc8681c74
Sema Ç <semacelikten22@gmail.com>
2024-08-20 14:44:55 +0300

    Şifre sıfırlama bağlantısı eklendi.

--- webTemplate/resources/views/auth/login.blade.php
+++ webTemplate/resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="tr">
 
 <head>
 
@@ -78,7 +78,7 @@
 
                         <div class="form-group mb-0 row mt-2">
                             <div class="col-sm-7 mt-3">
-                                <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock"></i> Forgot your password?</a>
+                                <a href="{{route('password.request')}}" class="text-muted"><i class="mdi mdi-lock"></i> Forgot your password?</a>
                             </div>
                             <div class="col-sm-5 mt-3">
                                 <a href="{{route('register')}}" class="text-muted"><i class="mdi mdi-account-circle"></i> Create an account</a>
