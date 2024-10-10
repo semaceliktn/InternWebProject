28a81093ba4ad19b1a3bf4aee3c5463b6a1a41cb
Sema Ç <semacelikten22@gmail.com>
2024-10-10 13:04:03 +0300

    Aktif blog içerikleri anasayfa_blog.blade.php sayfasında listelendi, IliskİBlogCategory ilişkisi ile kategori adı bilgisi kullanıldı ve tarih formatlaması uygulandı.

---
+++ webTemplate/resources/views/frontend/anasayfa/anasayfa_blog.blade.php
@@ -0,0 +1,32 @@
+@php
+$icerikler=App\Models\BlogContent::where('durum',1)->orderBy('sirano','ASC')->limit(3)->get();
+@endphp
+<section class="blog">
+    <div class="container">
+        <div class="row gx-0 justify-content-center">
+            @foreach($icerikler as $icerik)
+            <div class="col-lg-4 col-md-6 col-sm-9">
+                <div class="blog__post__item">
+                    <div class="blog__post__thumb">
+                        <a href="blog-details.html"><img src="{{asset('frontend/assets/img/blog/blog_post_thumb01.jpg')}}" alt=""></a>
+                        <div class="blog__post__tags">
+                            <a href="blog.html">{{ $icerik->IliskiBlogCategory->kategori_adi }}</a>
+                        </div>
+                    </div>
+                    <div class="blog__post__content">
+                        <span class="date">{{ $icerik->created_at->tz('Europe/Istanbul')->format('d.m.Y') }}</span>
+                        <h3 class="title"><a href="blog-details.html">{{ $icerik->baslik }}</a></h3>
+                        <a href="blog-details.html" class="read__more">Devamı...</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <div class="blog__button text-center">
+            <a href="blog.html" class="btn">Tümünü gör...</a>
+        </div>
+    </div>
+</section>
+
+
+
