a0f1780a83ed505ded9d1b15c13547f533e6dc02
Sema Ç <semacelikten22@gmail.com>
2024-10-23 13:22:19 +0300

    MailGonder.php sayfasında construct ve build fonksiyonları yazıldı. Message modelinde boot fonksiyonu yazılarak, modelde yeni bir kayıt oluşturulduğunda e-posta gönderilmesi sağlandı.

--- webTemplate/app/Models/Message.php
+++ webTemplate/app/Models/Message.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Support\Facades\Mail;
+use Mail;
+use App\Mail\MailGonder;
 
 class Message extends Model
 {
     use HasFactory;
 
     protected $fillable=['adi','email','telefon','konu','mesaj'];
+
+    public static function boot(){
+        parent::boot();
+        static::created(function ($bilgi){
+            $adminEmail="sylus@gmail.com";
+            Mail::to($adminEmail)->send(new MailGonder($bilgi));
+        });
+    }
 }
